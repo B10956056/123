a86688e1995828fd8822162ad6b7351978ab757e
B10956056 <lm434485@gmail.com>
2022-03-07 12:06:52 +0800

    0000

--- 123/Class1.cs
+++ 123/Class1.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+namespace _123 { 
 
-namespace _123
+class Program
 {
-    public class Class1
+
+    static void Main(string[] args)
     {
+        double meal_cost = 100;
+        int tip_percent = 15;
+        int tax_percent = 8;
+
+        int a = (int)meal_cost;
+
+        int sum = tax_percent + tip_percent + a;
+        Console.WriteLine("用餐總費用:" + sum);
     }
 }
+    }
