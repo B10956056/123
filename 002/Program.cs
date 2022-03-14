da825b6a1a99ad32182261f85352734922577a1e
user <user@DESKTOP-6JBEF5K>
2022-03-14 12:05:30 +0800

    123456789

---
+++ 002/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 1; i < 10; i += 3)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    for (int k = i; k < (i + 3); k++)
+                    {
+                        Console.Write($"{k:##}*{j:##} ={k * j:##}\t");
+                    }
+
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}
---
+++ 003/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studenNo = { 1, 2, 3, 4, 5, };
+            String[] studentName = { "Jack", "Mary", "Tom", "Grace", "Alice" };
+            int[] computerScore = { 80, 65, 100, 98, 83 };
+            int[] programScore = { 75, 67, 93, 25, 82 };
+            int[] animationScore = { 60, 62, 91, 50, 87 };
+
+            while(true)
+            {
+                Console.WriteLine("輸入座號(1 - 5)");
+                var input = Console.ReadLine();
+                int inputStudentNo = int.Parse(input);
+                int arrayIndex = Array.IndexOf(studentNo, intputstudentNo);
+
+            }
+        }
+
+    }
+}
